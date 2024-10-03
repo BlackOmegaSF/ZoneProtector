760b43ce02a2b74c73b15b94af52a51321df2f2a
Kian <kian.leiner@gmail.com>
2024-10-02 18:21:24 -0700

    Fix enderman teleport protection

--- src/main/java/com/kleinercode/fabric/mixin/CheckEndermanTeleportMixin.java
+++ src/main/java/com/kleinercode/fabric/mixin/CheckEndermanTeleportMixin.java
@@ -3,6 +3,7 @@ package com.kleinercode.fabric.mixin;
 import com.kleinercode.fabric.StateSaverAndLoader;
 import com.kleinercode.fabric.Zone;
 import com.kleinercode.fabric.ZoneProtector;
+import com.llamalad7.mixinextras.sugar.Local;
 import net.minecraft.entity.mob.EndermanEntity;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.math.BlockPos;
@@ -18,16 +19,16 @@ public abstract class CheckEndermanTeleportMixin {
 
     // Prevent enderman from teleporting into protected zones
 
-    @Inject(method = "teleportTo(DDD)Z", at = @At(value = "INVOKE_ASSIGN", target = "Lnet/minecraft/util/math/BlockPos$Mutable;<init>(DDD)V", shift = At.Shift.BEFORE), cancellable = true)
-    private void onEndermanTeleportCheck(double x, double y, double z, CallbackInfoReturnable<Boolean> cir) {
+    @Inject(method = "teleportTo(DDD)Z", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/World;getBlockState(Lnet/minecraft/util/math/BlockPos;)Lnet/minecraft/block/BlockState;", ordinal = 1), cancellable = true)
+    private void onEndermanTeleportCheck(double x, double y, double z, CallbackInfoReturnable<Boolean> cir, @Local BlockPos.Mutable mutable) {
 
         StateSaverAndLoader serverState = StateSaverAndLoader.getServerState(((EndermanEntity)(Object)this).getServer());
         Identifier worldId = ((EndermanEntity)(Object)this).getWorld().getRegistryKey().getValue();
-        BlockPos position = new BlockPos((int) x, (int) y, (int) z);
+        //BlockPos position = new BlockPos((int) x, (int) y, (int) z);
         for (Zone zone : serverState.zones) {
-            if (zone.containsPosition(worldId, position)) {
+            if (zone.containsPosition(worldId, mutable)) {
                 // Teleport needs to be cancelled
-                ZoneProtector.LOGGER.debug("Cancelling enderman teleport within zone {}", zone.prettyPrint());
+                //ZoneProtector.LOGGER.debug("Cancelling enderman teleport within zone {}", zone.prettyPrint());
                 cir.setReturnValue(false);
                 return;
             }
